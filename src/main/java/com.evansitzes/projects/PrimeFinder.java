7d85a7cdd36f38f7c3e8dd4d315984c0be080cd4
Evan <evan@simplymeasured.com>
2017-12-20 19:12:39 -0600

    Refactor project structure + gitignore

--- src/main/java/com.evansitzes/projects/PrimeFinder.java
+++ src/main/java/com.evansitzes/projects/PrimeFinder.java
@@ -4,25 +4,25 @@ public class PrimeFinder {
     private static final int MAX_NUMBER = 100;
 
     public static void main(String[] args) {
-    	int counter = 0;
+        int counter = 0;
 
         for (int i = 2; i <= MAX_NUMBER; i++) {
-        	boolean isPrime = true;
+            boolean isPrime = true;
 
             for(int j = 2; j < i; j++) {
 
                 if (i % j == 0) {
-        			isPrime = false;
-        			break;
+                    isPrime = false;
+                    break;
                 }
             }
 
             if (isPrime) {
-            	System.out.println("Prime number: " + i);
-            	counter++;
-        	}
+                System.out.println("Prime number: " + i);
+                counter++;
+            }
         }
 
-		System.out.println("Total number of primes between 1 and " + MAX_NUMBER + ": " + counter);        
+        System.out.println("Total number of primes between 1 and " + MAX_NUMBER + ": " + counter);
     }
 }
